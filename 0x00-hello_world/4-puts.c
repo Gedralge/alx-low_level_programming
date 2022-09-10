3b3d0676c9a946807914655e9d1dd749c63a2d25
Gedralge <Gedralgerrol@gmail.com>
2022-09-09 18:41:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
-#!/bin/bas
-#include <stdio.h>
+ #!/bin/bash
+1  #include <stdio.h>
 
-/**
- *  main - Entry point
- * 
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle"
-	    );
-	return (0);
-}
-                      
+2   /**
+3   *  main - Entry point
+4   *
+5   *Description: a c program that prints with put function
+6   *
+7   *Return: Always 0(Success)
+8   */
+9   int main(void)
+10  {
+11        puts("\"programmimng is like building amultilingual puzzle");
+
+12        return (0);
+
+13   }
+                  
