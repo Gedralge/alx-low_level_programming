953f621876a7ab20db33e683f129bfea7d0ca523
Gedralge <Gedralgerrol@gmail.com>
2022-09-09 19:01:28 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+1 #include <stdio.h>
+
+2  /**
+
+3  *main - Entry point
+
+4  *
+                                                            
+5  *Description: a c program that prints with put function
+
+6   *
+
+7  *Return: Always 0(Success)
+
+10 {
+
+11       printf("with proper grammar, but the outcome is apiece of art, \n");
+
+12        return (0);
