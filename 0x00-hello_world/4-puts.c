7adf9c0008a6c3aac74fa11b4609bb81802c8659
Gedralge <Gedralgerrol@gmail.com>
2022-09-11 14:54:54 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Return: Always (Success)
+ *Return: Always 0
  */
 int main(void)
-
 {
 	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
-}	
+}
