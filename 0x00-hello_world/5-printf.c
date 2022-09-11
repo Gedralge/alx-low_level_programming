f0040da7fad7ce9143b2aed16fe521573571c13d
Gedralge <Gedralgerrol@gmail.com>
2022-09-11 13:41:10 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,14 @@
-#!/bin/bash
-1 #include <stdio.h>
-
-2  /**
-
-3  *main - Entry point
-
-4  *
-                                                            
-5  *Description: a c program that prints with put function
-
-6   *
-
-7  *Return: Always 0(Success)
-
-10 {
-
-11       printf("with proper grammar, but the outcome is apiece of art, \n");
-
-12        return (0);
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Description: a c program that prints with put function
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art, |n");
+	return (0);
+}
