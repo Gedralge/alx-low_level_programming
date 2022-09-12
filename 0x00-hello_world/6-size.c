e0a98e801214ca2b22a970344e6c6fa02af79f81
Gedralge <Gedralgerrol@gmail.com>
2022-09-12 02:45:52 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float))
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
         return (0);
 }
