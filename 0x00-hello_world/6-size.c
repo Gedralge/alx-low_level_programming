a7024e98ee8e02638e409d7a3394e993dae12366
Gedralge <Gedralgerrol@gmail.com>
2022-09-12 01:00:27 -0700

    Task 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,32 +1,14 @@
-#!/bin/bash
-1  #include <stdio.h>
-
-2
-
-3  /**
-
-4   * main - Entry point
-
-5   *
-
-6  * Return: Always 0 (success)
-
-7  */
-
-8  int main(void)
-
-9  {
-
-10           int a;
-11           long int b;
-12           long long int c;
-13           char d;
-14           float f;
-15
-16           printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-17           printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-18           printf("size of a long int: %lu byte(s)\n", (usigned long)sizeof(b));
-19           printf("size of a long long int: %lu byte(s)\n," (unsigned long)sizeof(c));
-20           printf("size of a float int: %lu byte(s)\n", (unsigned long)sizeof(f));
-21           return (0);
-22    }
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %id byte(s)\n", sizeof(char));
+	printf("Size of an int: %id byte(s)\n", sizeof(int));
+	printf("Size of a long int: &id byte(s)\n", sizeof(lonf int));
+	printf("Size of a long long int: %id byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %id byte(s)\n", sizeof(float));
+	             return (0);
+}
