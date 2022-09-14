fe0fe92226c011a2f3a948fbeb50e1dfa0e36df0
Gedralge <Gedralgerrol@gmail.com>
2022-09-14 12:55:25 -0700

    Main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifindef HOKBERTON_HINCLUDED
-#defined HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
@@ -9,9 +9,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_baver(void);
+void jack_bauer(void);
 void times_table(void);
-int add(int a, intb);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
