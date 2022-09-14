4e98838c55d911b39221bd4737b489052ad6e94b
Gedralge <Gedralgerrol@gmail.com>
2022-09-14 13:06:34 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
-  *main - Entry point
-  *
-  *Description: a c program that prints with put function
-  *
- *Return: Always 0 (Success)
- */
+*main - Entry point
+*
+*Description: a c program that prints with put function
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	/* use sizeof() to get the size of dataatypes */
-
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
