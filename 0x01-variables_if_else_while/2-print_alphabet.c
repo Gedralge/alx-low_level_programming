fee8072332d8f77314cb0b033e536f45e2d875cf
Gedralge <Gedralgerrol@gmail.com>
2022-09-14 06:17:22 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabets
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
