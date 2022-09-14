96bfa6f06ae228a100664e1e79bd89d5a535da23
Gedralge <Gedralgerrol@gmail.com>
2022-09-14 10:53:21 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers starting from 0
+ *
+ * Return: return zero at the end
+ */
+int main(void)
+{
+	int i, j;
+
+
+	for (i = 0; i <= 0; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
