adbd5f0a4c80ad4f896b77e1956ec465d67ef8a2
Gedralge <Gedralgerrol@gmail.com>
2022-09-14 09:46:55 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints aseries of numbers with commas
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar('.');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
