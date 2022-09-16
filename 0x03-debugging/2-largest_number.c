89b5e1d9ed8f22b1ca00b8ef55b29a51ea9d6496
Gedralge <Gedralgerrol@gmail.com>
2022-09-15 18:53:11 -0700

    2-lrgest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
@@ -26,5 +26,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
-
