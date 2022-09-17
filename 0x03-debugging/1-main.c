a3f856e1ac3ed24cdcbf8c452a66e7b71b51347a
Gedralge <Gedralgerrol@gmail.com>
2022-09-17 10:39:56 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,12 +3,14 @@
 /**
  * main - causes an infinite loop
  * Return: 0
+ *
  */
+
 int main(void)
 {
 	int i;
 
-	printf(" infinite loop incoming :(\n");
+	printf(" Infinite loop incoming :(\n");
 
 	i = 0;
 	/**
@@ -17,9 +19,9 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
 	printf(" Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
+
 
