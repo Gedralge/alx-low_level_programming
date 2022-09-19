295d506427d1678d560ef628a3863a2f52d8e232
Gedralge <Gedralgerrol@gmail.com>
2022-09-19 13:02:56 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - length of string
+ * @dtr:char
+ * Return:int
+ */
+void puts_half(char *str)
+{
+	int i, k;
+
+	i = 0;
+	while (str[i])
+		i++;
+	k = i / 2;
+	if (i % 2)
+		k = 1;
+	while (k < i)
+	{
+		_putchar(str[k]);
+		k++;
+	}
+	_putchar('\n');
+}
+
