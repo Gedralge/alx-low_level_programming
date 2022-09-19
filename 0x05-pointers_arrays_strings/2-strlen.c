49c8629640f0778dd8cdfd3e02ecff8f71fa83be
Gedralge <Gedralgerrol@gmail.com>
2022-09-19 10:33:28 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s:char
+ * Return:int
+ */
+int _strlen(char *s)
+{
+	int p;
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		continue;
+	}
+	return (p);
+}
+
