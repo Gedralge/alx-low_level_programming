ebaed92700b2159b9eb37d4ba8e7ff065211d44f
Gedralge <Gedralgerrol@gmail.com>
2022-09-22 01:59:54 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	while (op == 0)
 
 	{
-		if ((*(s1 + i) == '0') && (*(s2 + i) == '0'))
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
 		op = *(s1 + i) - *(s2 + i);
 		i++;
