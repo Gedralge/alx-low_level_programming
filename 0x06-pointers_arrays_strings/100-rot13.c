a83406cfb697c13d0595caf9703af4434772e7e9
Gedralge <Gedralgerrol@gmail.com>
2022-09-21 20:26:32 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: inpute string
+ * Return: the pointer to dest
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, i;
+
+	char alphabet[] = "abcdefghijklmNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdeefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
+
