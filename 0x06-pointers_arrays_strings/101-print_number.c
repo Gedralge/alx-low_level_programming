43c5ab0600eadff4a5fb90490c00fcee66f139d0
Gedralge <Gedralgerrol@gmail.com>
2022-09-22 01:42:07 -0700

    main.h

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,47 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - prints an integer
+ * print_number - prints an integer using only _putchar
  * @p: input integer
  * Return: no return
  */
 
 void print_number(int p)
 {
-	unsigned int m, n, count;
+	int power;
+	int neg;
+	int hold;
 
+	neg = 0;
+	power = 1;
+	hold = p;
 	if (p < 0)
 	{
-		_putchar(45);
-		m = p * -1;
-	}
-	else
-	{
-		m = p;
-	}
-	n = m;
-	count = 1;
-	while (n > 9)
-	{
-		n /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
-}
+
+		while (hold > 9 || hold < -9)
+		{
+			power *= 10;
+			hold /= 10;
+		}
+
+		while (power > 0)
+		{
+			if (!neg)
+					_putchar((p / power % 10);
+						else
+						_putchar((p / power % 10) * -1 + '0');
+
+						power /= 10;
+						}
+						if (power == 1)
+						{
+						if (neg)
+						_putchar(p % 10) * -1 + '0');
+					else
+					_putchar(p % 10 + '0');
+					power = 0;
+					}
+					}
+					}
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,8 +3,9 @@
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char _strncat(char *dest, char *src, int n);
-int *_strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
@@ -12,3 +13,4 @@ char *leet(char *);
 
 
 #endif
+
