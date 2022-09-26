c87f846fc80f086b6d0f968d576b4f41af9ca4bc
Gedralge <Gedralgerrol@gmail.com>
2022-09-26 11:15:12 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,35 +6,34 @@
  */
 
 int main(void)
-{
-	int i = 1;
+	int i;
 
 	while (i <= 100)
+{
+	if (i % 3 == 0 && i % 5 == 0)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%i", i);
-		}
-		if (i != 100)
-		{
-			putchar(' ');
-		}
-		i++;
+		printf("FizzBuzz");
 	}
-	putchar('\n');
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%i", i);
+	}
+	if (i != 100)
+	{
+		putchar(' ');
+	}
+	i++;
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
 
