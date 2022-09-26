b9be99aa6563adffe16c1002d437e86f6b0b922c
Gedralge <Gedralgerrol@gmail.com>
2022-09-26 09:17:31 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of aprefix substring
+ * _strspn - gets the length of a prefix substring
  *
  * @s: string to work on
  * @accept: pattern to match against
- * Return: unsigned int to how many inintial bytes match the pattern
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, len = 0, match = 0;
+	unsigned int i, j, k, flag;
 
+	k = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
+		{
 			if (s[i] == accept[j])
 			{
-				len++;
-				break;
+				k++;
+
+				flag = 1;
 			}
+		}
+		if (flag == 0)
+		{
+			return (k);
+		}
 	}
-	if (len != 0)
-		match++;
-	if (match != len)
-		return (len);
+	return (0);
 }
 
