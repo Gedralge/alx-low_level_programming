4ec27956f8442c7a2fefc47f047248cbb2eedcd7
Gedralge <Gedralgerrol@gmail.com>
2022-10-20 11:41:25 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", h->len h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		gerrol++;
 
