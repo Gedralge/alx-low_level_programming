08d4a9d95fe7d0c3c62c7cde3708304841e87cb5
Gedralge <Gedralgerrol@gmail.com>
2022-10-25 12:36:02 -0700

    print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,13 +11,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0) || (year % 400 == 0 && year % 4 == 0))
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		if (month >= 2 && day >= 60)
-		{
+		if (month > 2)
 			day++;
-		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -25,11 +22,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid dte: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
+			printf("Day of the yesr: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
