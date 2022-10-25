00cf9c82014dbfb627cabef1f11b3817b8c197b6
Gedralge <Gedralgerrol@gmail.com>
2022-10-25 12:03:48 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
 
-	printf("Infinte loop avoided! \\o/\n");
+	/* while (i < 10) */
+	/* { */
+	/* putchar(i); */
+	/* } */
+
+	printf("Iunfinite loop avoided! \\o/\n");
 
 	return (0);
 }
