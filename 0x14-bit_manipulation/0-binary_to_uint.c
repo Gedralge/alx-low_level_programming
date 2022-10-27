8c86ec3913019172c949d00336e68c83c8688139
Gedralge <Gedralgerrol@gmail.com>
2022-10-27 11:20:56 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	count = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	for (len--, temp *= 2)
+	for (len--, temp = 1; len >= 0; len--, temp *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
