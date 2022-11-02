56e8bac4fdc9ca97b789419bc0a569abdd051e4b
Gedralge <Gedralgerrol@gmail.com>
2022-11-01 23:29:39 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,21 +6,21 @@
  * @filename: name of file to create
  * @text_content: string to write to file
  *
- * Return: 1 on sucess OR -1 on failure
+ * Return: 1 on success OR -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file, write_status, words = 0;
 
-	if (filename == NULL) /*checkif filename is present */
+	if (filename == NULL) /*check if filename is present*/
 		return (-1);
 
-	/* open file by creating it and if it exists write but truncate to 0 */
+	/* open file by creating it and if it exists write but truncate to 0*/
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == -1) /* check if file creation was success */
+	if (file == -1) /* check if file creation was success*/
 		return (-1);
 
-	if (text_content) /* write content to file if its not NULL */
+	if (text_content) /* write content to file if its not NULL*/
 	{
 		while (text_content[words] != '\0') /* find number of words*/
 			words++;
@@ -30,8 +30,8 @@ int create_file(const char *filename, char *text_content)
 		if (write_status == -1) /* check if write was a success*/
 			return (-1);
 	}
-	
+
 	close(file); /*close file*/
-			return (1);
-			}
+	return (1);
+}
 
