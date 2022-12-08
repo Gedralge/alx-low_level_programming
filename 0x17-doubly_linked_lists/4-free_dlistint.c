3185ebdefdc9d929e041f54575d23a6c92ddfeb4
Gedralge <Gedralgerrol@gmail.com>
2022-12-08 11:38:32 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked lit
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
