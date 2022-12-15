b1c8c1eab5e11d02a5bddcf8da69c6ac6ecea217
Gedralge <Gedralgerrol@gmail.com>
2022-12-15 11:14:07 -0800

    2

---
+++ 0x17-doubly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a doubly linked list
+ * @head: double pointer to the list
+ * @n: data to insert in the new node
+ * Return: the address of the new element, or NULL if if failed
+ */
+dlistint_t *add_dnodeint_end - adds a new node at the end of a doubly linked list
+@head: double          
