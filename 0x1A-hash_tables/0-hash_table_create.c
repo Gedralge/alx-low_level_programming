7ec4ba82ab62098d9829b83b5ec5b55173cfae17
Gedralge <Gedralgerror@mail.com>
2023-01-07 11:52:26 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t  *ht;
+
+	if (size == 0)
+		return (NULL);
+	ht = malloc(1 * sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
+
