11e417e4596a1b000821a6fa08f8de1ec2bc0896
Gedralge <Gedralgerror@mail.com>
2023-01-07 12:45:40 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,36 @@
 #include "hash_tables.h"
 
 /**
+ * free_list - frees a linked list
+ * @head: hash_node_t list to be freed
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+}
+
+/**
  * hash_table_delete - deletes a hash table
- * @ht: the given hash table
- *
- * Return: void
+ * @ht: hash table to be deleted
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *tmp, *cursor;
 
 	if (!ht)
 		return;
+
 	for (i = 0; i < ht->size; i++)
-	{
-		cursor = ht->array[i];
-		while (cursor)
-		{
-			tmp = cursor->next;
-			free(cursor->key);
-			free(cursor->value);
-			free(cursor);
-			cursor = tmp;
-		}
-	}
+		free_list(ht->array[i]);
 	free(ht->array);
 	free(ht);
 }
