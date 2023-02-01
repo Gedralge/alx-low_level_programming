f5393193fd3e17e94c491dfb5d33225a56763cc9
Gedralge <Gedralgerror@mail.com>
2023-02-01 10:24:35 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,9 +25,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	while (temp->next != NULL)
 		temp = temp->next;
-	temp->next = new_node;
+	temp-next = new_node;
 	new_node->prev = temp;
-	
 	return (new_node);
 }
 
